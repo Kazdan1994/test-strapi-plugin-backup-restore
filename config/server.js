08c11db09fcdc26f0e8a8423775f3757b4f210a6
arthur.jacquemin <arthurjacquemin2@gmail.com>
2021-07-29 17:20:26 +0200

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '7415d4f208f38771471a978984a522d1'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '670ffc3b-a3bb-4d80-9e74-cd4d2e77f341'
+};
